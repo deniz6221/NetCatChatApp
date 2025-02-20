099e2c2cd78a801597526dba8a094e16b1ff4998
Deniz <160037328+deniz6221@users.noreply.github.com>
2025-02-20 17:17:46 +0300

    Some renders, threading not implemented yet

--- program.py
+++ program.py
@@ -27,6 +27,7 @@ def render_online_users(online_users):
         else:    
             print(f"{i + 1}. {user['name']} ({user['unread_messages']} unread messages)")
 
+
 def send_json(ip, message):
     messageJson = json.dumps(message)
     message = subprocess.Popen([nc_command, ip, "40000"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -58,15 +59,20 @@ for discover in discovers:
     try:
         message = json.loads(line.strip())
         if (message["type"] == "REPLY_DISCOVER"):
-            online_users.append({"ip": message["reply_ip"], "name": message["reply_name"], "unread_messages": 0})
+            online_users.append({"ip": message["reply_ip"], "name": message["reply_name"], "unread_messages": 0, "process": discover, "messages": []})
     except:
         discover.kill()
-        pass        
-
+        pass
+    
     
 
 
 while True:
+    for user in online_users:
+        if user["process"].poll() != None:
+            online_users.remove(user)
+            render_online_users(online_users)
+            break
     output = server.stdout.readline().decode()
     if output:
         try:
@@ -75,10 +81,32 @@ while True:
             message = json.loads(message)
             message_type = message["type"]
             if (message_type == "DISCOVER"):
-                online_users.append({"ip": message["sender_ip"], "name": message["sender_name"], "unread_messages": 0})
-                reply_discover = send_json(message["sender_ip"], {"type": "REPLY_DISCOVER", "reply_ip": my_ip, "reply_name": username})
+                sender_ip = message["sender_ip"]
+                new_process = subprocess.Popen([nc_command, sender_ip, "40000"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                online_users.append({"ip": sender_ip, "name": message["sender_name"], "unread_messages": 0, "process": new_process, "messages": []})
+                reply_discover = send_json(sender_ip, {"type": "REPLY_DISCOVER", "reply_ip": my_ip, "reply_name": username})
                 render_online_users(online_users)
         except:
             pass        
+    if len(online_users) > 0:
+        render_online_users(online_users)
+        user_index = input("Enter user index to send or enter R to refresh: ")
+        while (not user_index.isdigit() and user_index != "R") or ((user_index != "R") and (int(user_index) < 1 or int(user_index) > len(online_users))):
+            print("Invalid user index")
+            user_index = input("Enter user index to send or enter R to refresh: ")
+        if user_index == "R":
+            continue
+    
+        user_index = int(user_index)
+        user = online_users[user_index - 1]
+        user["unread_messages"] = 0
 
+        for message in user["messages"]:
+            print(f"Chat with {user['name']}:")
+            print(f"{message['sender']}: {message['message']}")
+            input()
+            
+
+        message = input("Enter message: ")
+        send_json(user["ip"], {"type": "MESSAGE", "sender": username, "message": message})        
     time.sleep(0.01)    
