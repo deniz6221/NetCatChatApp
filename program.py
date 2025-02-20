0b3c606d1005477f27543fe662a468876e28c77a
Deniz <160037328+deniz6221@users.noreply.github.com>
2025-02-20 20:01:30 +0300

    Program split into threads,now renders are done in parallel

--- program.py
+++ program.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import time
 import socket
+import threading
 
 global nc_command
 nc_command = "ncat" if os.name == "nt" else "nc"
@@ -28,11 +29,10 @@ def render_online_users(online_users):
             print(f"{i + 1}. {user['name']} ({user['unread_messages']} unread messages)")
 
 
-def send_json(ip, message):
+def send_json(user, message):
     messageJson = json.dumps(message)
-    message = subprocess.Popen([nc_command, ip, "40000"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    message.stdin.write(messageJson.encode())
-    message.stdin.close()
+    user["process"].stdin.write(messageJson.encode())
+
 
 username = input("Enter your name: ")
 server = subprocess.Popen([nc_command, '-lknp', '40000'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -64,49 +64,121 @@ for discover in discovers:
         discover.kill()
         pass
     
-    
+renderState = 1
+active_user = -1
+thread_lock = threading.Lock()
 
 
-while True:
-    for user in online_users:
-        if user["process"].poll() != None:
-            online_users.remove(user)
-            render_online_users(online_users)
-            break
-    output = server.stdout.readline().decode()
-    if output:
-        try:
-            message = output.strip()
-            print(message)
-            message = json.loads(message)
-            message_type = message["type"]
-            if (message_type == "DISCOVER"):
-                sender_ip = message["sender_ip"]
-                new_process = subprocess.Popen([nc_command, sender_ip, "40000"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                online_users.append({"ip": sender_ip, "name": message["sender_name"], "unread_messages": 0, "process": new_process, "messages": []})
-                reply_discover = send_json(sender_ip, {"type": "REPLY_DISCOVER", "reply_ip": my_ip, "reply_name": username})
+def renderThread():
+    global renderState
+    global active_user
+    while True:
+        with thread_lock:
+            if renderState == 1:
+                print("\n\n\n")
                 render_online_users(online_users)
-        except:
-            pass        
-    if len(online_users) > 0:
-        render_online_users(online_users)
-        user_index = input("Enter user index to send or enter R to refresh: ")
-        while (not user_index.isdigit() and user_index != "R") or ((user_index != "R") and (int(user_index) < 1 or int(user_index) > len(online_users))):
-            print("Invalid user index")
-            user_index = input("Enter user index to send or enter R to refresh: ")
-        if user_index == "R":
-            continue
-    
-        user_index = int(user_index)
-        user = online_users[user_index - 1]
-        user["unread_messages"] = 0
+                if (len(online_users) != 0):
+                    print("Enter user index to view chat: ")
+                renderState = 0
+            elif renderState == 3:
+                print("\n\n\n")
+                print(f"Chat with {online_users[active_user]['name']}:")
+                for message in online_users[active_user]["messages"]:
+                    print(f"{message['sender']}: {message['message']}")
+                print("Enter message or enter Q to go back to previous menu: ")    
+                renderState = 2            
+        time.sleep(0.3)
 
-        for message in user["messages"]:
-            print(f"Chat with {user['name']}:")
-            print(f"{message['sender']}: {message['message']}")
-            input()
-            
+def disconnectThread():
+    global renderState
+    global active_user
+    while True:
+        with thread_lock:
+            for i, user in enumerate(online_users):
+                if user["process"].poll() != None:
+                    online_users.remove(user)
+                    if renderState == 0:
+                        renderState = 1
+                    if (renderState == 2 or renderState == 3) and active_user == i:
+                        renderState = 1
+                    break
+        time.sleep(1)
 
-        message = input("Enter message: ")
-        send_json(user["ip"], {"type": "MESSAGE", "sender": username, "message": message})        
-    time.sleep(0.01)    
+def serverThread():
+    global renderState
+    global active_user
+    while True:
+        output = server.stdout.readline().decode()
+        if output:
+            try:
+                message = output.strip()
+                print(message)
+                message = json.loads(message)
+                message_type = message["type"]
+                if (message_type == "DISCOVER"):
+                    sender_ip = message["sender_ip"]
+                    new_process = subprocess.Popen([nc_command, sender_ip, "40000"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    print("Discover message received")
+                    
+                    with thread_lock:
+                        print("Lock acquired for online users list")
+                        online_users.append({"ip": sender_ip, "name": message["sender_name"], "unread_messages": 0, "process": new_process, "messages": []})
+                        send_json(online_users[-1], {"type": "REPLY_DISCOVER", "reply_ip": my_ip, "reply_name": username})
+                        if renderState == 0:
+                            renderState = 1
+                elif (message_type == "MESSAGE"):
+                    sender_ip = message["sender_ip"]
+                    message = message["message"]
+                    sender_name = message["sender_name"]
+                    with thread_lock:
+                        for user in online_users:
+                            if user["ip"] == sender_ip:
+                                user["messages"].append({"sender": sender_name, "message": message})
+                                if renderState != 2 and renderState != 3:
+                                    user["unread_messages"] += 1
+                                else:    
+                                    renderState = 3                             
+            except:
+                pass
+
+def inputThread():
+    global renderState
+    global active_user
+    while True:
+        userInput = input()
+        with thread_lock:
+            if renderState == 0 or renderState == 1:
+                if len(online_users) <= 0:
+                    continue
+                if (not userInput.isdigit()) or (int(userInput) < 1 or int(userInput) > len(online_users)):
+                    print("Invalid user index")
+                    continue
+                active_user = int(userInput) - 1
+                renderState = 3
+            else:
+                message = userInput
+                if message == "Q":
+                    renderState = 1
+                else:
+                    online_users[active_user]["messages"].append({"sender": username, "message": message})
+                    send_json(online_users[active_user], {"type": "MESSAGE", "sender_ip": my_ip, "sender_name": username, "message": message})
+                    renderState = 3
+        time.sleep(0.5)        
+
+serverThread = threading.Thread(target=serverThread)
+serverThread.daemon = True
+serverThread.start()
+
+renderThread = threading.Thread(target=renderThread)
+renderThread.daemon = True
+renderThread.start()
+
+disconnectThread = threading.Thread(target=disconnectThread)
+disconnectThread.daemon = True
+disconnectThread.start()
+
+inputThread = threading.Thread(target=inputThread)
+inputThread.daemon = True
+inputThread.start()
+
+inputThread.join()
